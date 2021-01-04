29008ef14ffe67713a3d464dcaf8812afd3661c4
riccardosacco <rickybag99@gmail.com>
2021-01-04 12:31:35 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h3>Stopwatch</h3>
+    </div>
+  );
+};
+
+// 1 - 0,989
+// 2 - 1,266
+
+export default App;
